f751f943fb6c72b1bb10c6dde567b4753ed37647
Stefan Heimes <stefan_heimes@hotmail.com>
2013-02-13 21:30:29 +0100

    Update DCA

--- system/modules/syncCtoPro/dca/tl_syncCto_clients_syncTo.php
+++ system/modules/syncCtoPro/dca/tl_syncCto_clients_syncTo.php
@@ -9,9 +9,11 @@
  * @filesource
  */
 
-$GLOBALS['TL_DCA']['tl_syncCto_clients_syncTo']['palettes']['default'] = preg_replace(
-        "/{table_legend},database_check/i", "{table_legend},database_check,database_pages_check", $GLOBALS['TL_DCA']['tl_syncCto_clients_syncTo']['palettes']['default']
-);
+$GLOBALS['TL_DCA']['tl_syncCto_clients_syncTo']['palettes']['__selector__'][] = 'database_check';
+
+$GLOBALS['TL_DCA']['tl_syncCto_clients_syncTo']['subpalettes']['database_check'] = 'database_pages_check';
+
+$GLOBALS['TL_DCA']['tl_syncCto_clients_syncTo']['fields']['database_check']['eval']['submitOnChange'] = 'true';
 
 $GLOBALS['TL_DCA']['tl_syncCto_clients_syncTo']['fields']['database_pages_check'] = array(
     'label'     => &$GLOBALS['TL_LANG']['tl_syncCto_clients_syncTo']['database_pages_check'],
