18dbca93f68dcc4eadd640ab67b6ad40dd473279
Stefan heimes <stefan_heimes@hotmail.com>
2013-02-18 18:20:34 +0100

    Update Settings

--- system/modules/syncCtoPro/dca/tl_syncCto_settings.php
+++ system/modules/syncCtoPro/dca/tl_syncCto_settings.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * Contao Open Source CMS
@@ -8,58 +8,241 @@
  * @license    EULA
  * @filesource
  */
-
 $GLOBALS['TL_DCA']['tl_syncCto_settings']['palettes']['default'] = preg_replace(
         "/syncCto_hidden_tables/i", "syncCto_hidden_tables;{diff_legend:hide},syncCto_diff_blacklist,syncCto_sync_blacklist", $GLOBALS['TL_DCA']['tl_syncCto_settings']['palettes']['default']
 );
 
 $GLOBALS['TL_DCA']['tl_syncCto_settings']['fields']['syncCto_diff_blacklist'] = array(
-    'label' => &$GLOBALS['TL_LANG']['tl_syncCto_settings']['diff_blacklist'],
-    'exclude' => true,
+    'label'     => &$GLOBALS['TL_LANG']['tl_syncCto_settings']['diff_blacklist'],
+    'exclude'   => true,
     'inputType' => 'multiColumnWizard',
-    'eval' => array(
+    'eval'      => array(
+        'alwaysSave'   => true,
         'columnFields' => array(
             'table' => array(
-                'label' => &$GLOBALS['TL_LANG']['tl_syncCto_settings']['blacklist_table'],
-                'exclude' => true,
-                'options' => array('tl_page', 'tl_article', 'tl_content'),
-                'inputType' => 'select',                       
-                'eval' => array('includeBlankOption' => 'true', 'chosen' => 'true', 'style' => 'width:235px'),
+                'label'     => &$GLOBALS['TL_LANG']['tl_syncCto_settings']['blacklist_table'],
+                'reference' => &$GLOBALS['TL_LANG']['tl_syncCto_settings']['tables'],
+                'exclude'   => true,
+                'options'   => array('all', 'tl_page', 'tl_article', 'tl_content'),
+                'inputType' => 'select',
+                'eval'      => array(
+                    'includeBlankOption' => true,
+                    'chosen'             => true,
+                    'style'              => 'width:235px'),
             ),
-            'entry' => array(
-                'label' => &$GLOBALS['TL_LANG']['tl_syncCto_settings']['blacklist_entry'],
-                'exclude' => true,
-                'inputType' => 'text',                        
-                'eval' => array('style' => 'width:352px'),
+            'entry'              => array(
+                'label'     => &$GLOBALS['TL_LANG']['tl_syncCto_settings']['blacklist_entry'],
+                'exclude'   => true,
+                'inputType' => 'text',
+                'eval'      => array('style'         => 'width:352px'),
             )
         )
-    )
-    //'load_callback' => array(array('tl_syncCto_settings', 'loadDiffBlacklist')),
-    //'save_callback' => array(array('tl_syncCto_settings', 'saveMcwEntries'))
+    ),
+    'load_callback' => array(array('tl_syncCto_settings_pro', 'loadDiffBlacklist')),
+    'save_callback' => array(array('tl_syncCto_settings_pro', 'saveDiffBlacklist'))
 );
 
 $GLOBALS['TL_DCA']['tl_syncCto_settings']['fields']['syncCto_sync_blacklist'] = array(
-    'label' => &$GLOBALS['TL_LANG']['tl_syncCto_settings']['sync_blacklist'],
-    'exclude' => true,
+    'label'     => &$GLOBALS['TL_LANG']['tl_syncCto_settings']['sync_blacklist'],
+    'exclude'   => true,
     'inputType' => 'multiColumnWizard',
-    'eval' => array(
+    'eval'      => array(
+        'alwaysSave'   => true,
         'columnFields' => array(
             'table' => array(
-                'label' => &$GLOBALS['TL_LANG']['tl_syncCto_settings']['blacklist_table'],
-                'options' => array('tl_page', 'tl_article', 'tl_content'),
-                'inputType' => 'select',                      
-                'eval' => array('includeBlankOption' => 'true', 'chosen' => 'true', 'style' => 'width:235px'),
+                'label'     => &$GLOBALS['TL_LANG']['tl_syncCto_settings']['blacklist_table'],
+                'reference' => &$GLOBALS['TL_LANG']['tl_syncCto_settings']['tables'],
+                'options'   => array('all', 'tl_page', 'tl_article', 'tl_content'),
+                'inputType' => 'select',
+                'eval'      => array(
+                    'includeBlankOption' => true,
+                    'chosen'             => true,
+                    'style'              => 'width:235px'
+                ),
             ),
-            'entry' => array(
-                'label' => &$GLOBALS['TL_LANG']['tl_syncCto_settings']['blacklist_entry'],
-                'exclude' => true,
-                'inputType' => 'text',                        
-                'eval' => array('style' => 'width:352px'),
+            'entry'              => array(
+                'label'     => &$GLOBALS['TL_LANG']['tl_syncCto_settings']['blacklist_entry'],
+                'exclude'   => true,
+                'inputType' => 'text',
+                'eval'      => array(
+                    'style'         => 'width:352px'
+                ),
             )
         )
-    )
-    //'load_callback' => array(array('tl_syncCto_settings', 'loadSyncBlacklist')),
-    //'save_callback' => array(array('tl_syncCto_settings', 'saveMcwEntries'))
+    ),
+    'load_callback' => array(array('tl_syncCto_settings_pro', 'loadSyncBlacklist')),
+    'save_callback' => array(array('tl_syncCto_settings_pro', 'saveSyncBlacklist'))
 );
+
+/**
+ * Class for syncCto settings
+ */
+class tl_syncCto_settings_pro extends Backend
+{
+
+    /**
+     * Return a list with default values and 
+     * user values.
+     * 
+     * @param array $mixValues
+     * @return array
+     */
+    public function loadDiffBlacklist($mixValues)
+    {
+        $mixValues = (array) deserialize($mixValues);
+        $arrReturn = array();
+        $arrKnownValues = array();
+
+        // Get basic settings
+        foreach ($GLOBALS['SYC_CONFIG']['diff_blacklist'] as $strTableName => $arrValues)
+        {
+            foreach ($arrValues as $strField)
+            {
+                $arrReturn[] = array(
+                    'table' => $strTableName,
+                    'entry' => $strField
+                );
+
+                $arrKnownValues[] = $strTableName . '::' . $strField;
+            }
+        }
+
+        // Unset basic settings
+        foreach ($mixValues as $key => $arrValues)
+        {
+            $strCheckValue = $arrValues['table'] . '::' . $arrValues['entry'];
+
+            if (in_array($strCheckValue, $arrKnownValues))
+            {
+                unset($mixValues[$key]);
+            }
+        }
+
+        // Merge
+        $arrReturn = array_merge($arrReturn, $mixValues);
+
+        // Return
+        return $arrReturn;
+    }
+
+    /**
+     * Return a list with default values and 
+     * user values.
+     * 
+     * @param array $mixValues
+     * @return array
+     */
+    public function loadSyncBlacklist($mixValues)
+    {
+        $mixValues = (array) deserialize($mixValues);
+        $arrReturn = array();
+        $arrKnownValues = array();
+        
+        // Get basic settings
+        foreach ($GLOBALS['SYC_CONFIG']['sync_blacklist'] as $strTableName => $arrValues)
+        {
+            foreach ($arrValues as $strField)
+            {
+                $arrReturn[] = array(
+                    'table' => $strTableName,
+                    'entry' => $strField
+                );
+
+                $arrKnownValues[] = $strTableName . '::' . $strField;
+            }
+        }
+
+        // Unset basic settings
+        foreach ($mixValues as $key => $arrValues)
+        {
+            $strCheckValue = $arrValues['table'] . '::' . $arrValues['entry'];
+
+            if (in_array($strCheckValue, $arrKnownValues))
+            {
+                unset($mixValues[$key]);
+            }
+        }
+
+        // Merge
+        $arrReturn = array_merge($arrReturn, $mixValues);
+
+        // Return
+        return $arrReturn;
+    }
+
+    /**
+     * Return a list with default values and 
+     * user values.
+     * 
+     * @param array $mixValues
+     * @return array
+     */
+    public function saveDiffBlacklist($mixValues)
+    {
+        $mixValues      = (array) deserialize($mixValues);
+        $arrKnownValues = array();
+
+        // Get basic settings
+        foreach ($GLOBALS['SYC_CONFIG']['diff_blacklist'] as $strTableName => $arrValues)
+        {
+            foreach ($arrValues as $strField)
+            {
+                $arrKnownValues[] = $strTableName . '::' . $strField;
+            }
+        }
+
+        // Unset basic settings
+        foreach ($mixValues as $key => $arrValues)
+        {
+            $strCheckValue = $arrValues['table'] . '::' . $arrValues['entry'];
+
+            if (in_array($strCheckValue, $arrKnownValues))
+            {
+                unset($mixValues[$key]);
+            }
+        }
+
+        // Return
+        return serialize($mixValues);
+    }
+
+    /**
+     * Return a list with default values and 
+     * user values.
+     * 
+     * @param array $mixValues
+     * @return array
+     */
+    public function saveSyncBlacklist($mixValues)
+    {
+        $mixValues      = (array) deserialize($mixValues);
+        $arrKnownValues = array();
+        $arrReturn = array();
+
+        // Get basic settings
+        foreach ($GLOBALS['SYC_CONFIG']['sync_blacklist'] as $strTableName => $arrValues)
+        {
+            foreach ($arrValues as $strField)
+            {
+                $arrKnownValues[] = $strTableName . '::' . $strField;
+            }
+        }
+
+        // Unset basic settings
+        foreach ($mixValues as $key => $arrValues)
+        {
+            $strCheckValue = $arrValues['table'] . '::' . $arrValues['entry'];
+
+            if (!in_array($strCheckValue, $arrKnownValues))
+            {
+                $arrReturn[] = $arrValues;
+            }
+        }
+
+        // Return
+        return serialize($arrReturn);
+    }
+
+}
 
 ?>
--- system/modules/syncCtoPro/languages/de/tl_syncCto_settings.php
+++ system/modules/syncCtoPro/languages/de/tl_syncCto_settings.php
@@ -17,9 +17,16 @@ $GLOBALS['TL_LANG']['tl_syncCto_settings']['diff_legend']              = 'Diff-E
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_syncCto_settings']['diff_blacklist']           = array('Versteckte Tabellen im Diff', 'Hier können Sie auswählen, welche Tabelle nicht im Diff angezeigt werden soll.');
-$GLOBALS['TL_LANG']['tl_syncCto_settings']['sync_blacklist']           = array('Ausgeschlossene Tabellen in der Synchronisation', 'Hier können Sie auswählen, welche Tabelle niemals synchronisiert werden soll.');
+$GLOBALS['TL_LANG']['tl_syncCto_settings']['diff_blacklist']           = array('Versteckte Felder', 'Hier können Sie auswählen, welche Felder nicht im Diff angezeigt werden soll.');
+$GLOBALS['TL_LANG']['tl_syncCto_settings']['sync_blacklist']           = array('Ausgeschlossene Felder', 'Hier können Sie auswählen, welche Felder niemals synchronisiert werden soll.');
 $GLOBALS['TL_LANG']['tl_syncCto_settings']['blacklist_table']          = array('Datenbank-Tabelle', 'Hier können Sie auswählen, welche Tabelle verwendet werden soll.');
 $GLOBALS['TL_LANG']['tl_syncCto_settings']['blacklist_entry']          = array('Datenbank-Spalte', 'Hier können Sie die Spalte eintragen, die nicht verwendet werden soll.');
 
+/**
+ * References
+ */
+$GLOBALS['TL_LANG']['tl_syncCto_settings']['tables']['all']            = 'Alle';
+$GLOBALS['TL_LANG']['tl_syncCto_settings']['tables']['tl_page']        = 'Seitenstruktur';
+$GLOBALS['TL_LANG']['tl_syncCto_settings']['tables']['tl_article']     = 'Artikel';
+$GLOBALS['TL_LANG']['tl_syncCto_settings']['tables']['tl_content']     = 'Inhaltselement';
 ?>
