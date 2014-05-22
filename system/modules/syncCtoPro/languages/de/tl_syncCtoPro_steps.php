1603dae5071b35e8f44800696fe3def8e2edbf12
Stefan Heimes <stefan_heimes@hotmail.com>
2014-05-22 11:16:12 +0200

    Add the language data and templates for the news and events.

--- system/modules/syncCtoPro/languages/de/tl_syncCtoPro_steps.php
+++ system/modules/syncCtoPro/languages/de/tl_syncCtoPro_steps.php
@@ -24,6 +24,10 @@ $GLOBALS['TL_LANG']['tl_syncCtoPro_steps']['pro_sync']['error_client']  = 'syncC
 /**
  * Popup
  */
-$GLOBALS['TL_LANG']['tl_syncCtoPro_steps']['popup']['headline_detail']   = 'Unterschiede für %s (ID: %s) anzeigen';
-$GLOBALS['TL_LANG']['tl_syncCtoPro_steps']['popup']['missing']           = '-';
-$GLOBALS['TL_LANG']['tl_syncCtoPro_steps']['popup']['position']          = 'Seite: %s | Artikel: %s';
+$GLOBALS['TL_LANG']['tl_syncCtoPro_steps']['popup']['headline_detail'] = 'Unterschiede für %s (ID: %s) anzeigen';
+$GLOBALS['TL_LANG']['tl_syncCtoPro_steps']['popup']['missing']         = '-';
+$GLOBALS['TL_LANG']['tl_syncCtoPro_steps']['popup']['position']        = 'Seite: %s | Artikel: %s';
+$GLOBALS['TL_LANG']['tl_syncCtoPro_steps']['popup']['position_news']   = 'Newsarchiv: %s | Newstitel: %s';
+$GLOBALS['TL_LANG']['tl_syncCtoPro_steps']['popup']['position_events'] = 'Kalender: %s | Eventtitel: %s';
+$GLOBALS['TL_LANG']['tl_syncCtoPro_steps']['popup']['normal_content']  = 'Normaler Inhalt';
+$GLOBALS['TL_LANG']['tl_syncCtoPro_steps']['popup']['other_content']   = 'Zusätzlicher Inhalt für <span title="%s">%s</span>';
