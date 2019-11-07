f4ee06362832f9b5643b68de23060a9aa5e661fd
Stefan Heimes <stefan_heimes@hotmail.com>
2019-11-07 23:52:10 +0100

    Add tl_class w50 for the db sync

--- src/Resources/contao/dca/tl_syncCto_clients_syncTo.php
+++ src/Resources/contao/dca/tl_syncCto_clients_syncTo.php
@@ -3,7 +3,7 @@
 /**
  * Contao Open Source CMS
  *
- * @copyright  MEN AT WORK 2014 
+ * @copyright  MEN AT WORK 2014
  * @package    syncCtoPro
  * @license    EULA
  * @filesource
@@ -13,17 +13,22 @@ $GLOBALS['TL_DCA']['tl_syncCto_clients_syncTo']['palettes']['__selector__'][] = 
 
 // Check if we alread have a value in the subpallettes.
 $currentSubPalettes = $GLOBALS['TL_DCA']['tl_syncCto_clients_syncTo']['subpalettes']['database_check'];
-if(empty($currentSubPalettes)){
-    $GLOBALS['TL_DCA']['tl_syncCto_clients_syncTo']['subpalettes']['database_check']  = 'database_pages_check'; 
+if (empty($currentSubPalettes)) {
+    $GLOBALS['TL_DCA']['tl_syncCto_clients_syncTo']['subpalettes']['database_check'] = 'database_pages_check';
 } else {
-    $GLOBALS['TL_DCA']['tl_syncCto_clients_syncTo']['subpalettes']['database_check']  = sprintf("%s,database_pages_check", $currentSubPalettes); 
+    $GLOBALS['TL_DCA']['tl_syncCto_clients_syncTo']['subpalettes']['database_check'] = sprintf("%s,database_pages_check",
+        $currentSubPalettes);
 }
 
 $GLOBALS['TL_DCA']['tl_syncCto_clients_syncTo']['fields']['database_check']['eval']['submitOnChange'] = 'true';
 
 $GLOBALS['TL_DCA']['tl_syncCto_clients_syncTo']['fields']['database_pages_check'] = array
 (
-    'label'          => &$GLOBALS['TL_LANG']['tl_syncCto_clients_syncTo']['database_pages_check'],
-    'inputType'      => 'checkbox',
-    'exclude'        => true,
+    'label'     => &$GLOBALS['TL_LANG']['tl_syncCto_clients_syncTo']['database_pages_check'],
+    'inputType' => 'checkbox',
+    'exclude'   => true,
+    'eval'      =>
+        [
+            'tl_class' => 'w50'
+        ]
 );
